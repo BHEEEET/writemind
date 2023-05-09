a9a061f0a2bc181203677a9daeb20bd40006d525
BHEET Lappi Win <krkbheet@gmail.com>
2023-05-09 10:34:43 +0200

    add github api fetch

--- src/components/titles/titles.tsx
+++ src/components/titles/titles.tsx
@@ -1,9 +1,20 @@
 const TitleList = () => {
-    const QSDF = "qsdfmlkj"
+  fetch(`https://api.github.com/repos/KRKBHEET/writemind/contents/public/posts`, {
+    'headers': {
+      'Authorization': process.env.GITHUB_ACCES_TOKEN
+    }
+  })
+    .then((response) => response.json())
+    .then((GitPosts) => {
+      console.log(GitPosts)
+
+    });
+
+
     return (
       <>
       <ul>
-        <li><a href="/">{QSDF}</a></li>
+        <li><a href="/">{}</a></li>
       </ul>
       </>
     );
